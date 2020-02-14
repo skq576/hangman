6655a922d58c6b55b877a88205b1a6c42440619e
Mark Sanchez <sanch0220@gmail.com>
2020-02-13 23:05:55 -0600

    get json data; filter words

--- hangman.py
+++ hangman.py
@@ -1,7 +1,10 @@
-import PyDictionary as pyd
+#import PyDictionary as pyd
 import random
 import requests
 import json
+from os.path import dirname
+
+print(dirname(__file__))
 
 word_length_str = input("what length should the word be (3-10)?")
 try:
@@ -19,9 +22,30 @@ try:
 except:
     print("Please input valid number")
 
-dictionary = {}
-url = "https://github.com/adambom/dictionary/blob/master/dictionary.json"
+dictionary = []
+url = "https://raw.githubusercontent.com/adambom/dictionary/master/dictionary.json"
 r = requests.get(url)
-dictionary = r.json()
-print(dictionary[1])
+response = r.json()
+# filename = "c:/Users/mark/pyworkspace/hangman/output.json"
+# with open(filename, "w") as f:
+#     json.dump(response, f, indent=4)
+dictionary = list(response.keys())
+# print(len(dictionary[0]))
+
+def filter_words(word):
+    if len(word) == word_length:
+        return True
+    else:
+        return False
+available_words = []
+filtered_dict = filter(filter_words, dictionary)
+
+for word in filtered_dict:
+    available_words.append(word)
+print(available_words[0])
+print(available_words[1])
+print(len(available_words))
+
+
+
 
